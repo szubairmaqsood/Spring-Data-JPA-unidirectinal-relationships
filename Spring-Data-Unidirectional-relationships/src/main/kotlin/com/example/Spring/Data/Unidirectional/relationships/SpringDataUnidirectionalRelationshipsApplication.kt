24148b84ad71733fa7291149845f1c8e2b303788
zubair-pc\zubair <szubair@gmail.com>
2021-06-02 14:34:48 +0500

    Inital set up of project for unidirectional relationships

---
+++ Spring-Data-Unidirectional-relationships/src/main/kotlin/com/example/Spring/Data/Unidirectional/relationships/SpringDataUnidirectionalRelationshipsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Spring.Data.Unidirectional.relationships
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataUnidirectionalRelationshipsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDataUnidirectionalRelationshipsApplication>(*args)
+}
---
+++ Spring-Data-Unidirectional-relationships/src/test/kotlin/com/example/Spring/Data/Unidirectional/relationships/SpringDataUnidirectionalRelationshipsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Spring.Data.Unidirectional.relationships
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataUnidirectionalRelationshipsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
